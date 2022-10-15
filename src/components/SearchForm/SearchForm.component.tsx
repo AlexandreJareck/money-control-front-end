386a3d5c4de4f86b9c74463f596ae13cb6ce3849
AlexandreJareck <alexandrejareck@gmail.com>
2022-10-15 10:23:13 -0300

    feat: add hook-form and zod in seacrh form

--- src/components/SearchForm/SearchForm.component.tsx
+++ src/components/SearchForm/SearchForm.component.tsx
@@ -1,10 +1,34 @@
 import { SearchFormContainer } from './SearchForm.styles'
 import { Search } from 'react-feather'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
+const seachFormSchema = z.object({
+  query: z.string()
+})
+
+type SearchFormInputs = z.infer<typeof seachFormSchema>
+
 export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(seachFormSchema)
+  })
+
+  async function handleSearchTransaction(data: SearchFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Search transactions" />
-      <button type="submit">
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
+      <input type="text" placeholder="Search transactions" {...register('query')} />
+      <button type="submit" disabled={isSubmitting}>
         <Search />
         Search
       </button>
